30ab12cb7606db358837346d5b64beeffe8e6485
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-04-11 20:13:18 -0400

    criei Ex003

--- Ex003/index.php
+++ Ex003/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tipos primitivos em PHP</title>
+
     <!-- 
-        tipos primitivos
-        divido em ESCALARES, COMPOSTOS E ESPECIAIS.
+        tipos primitivos: divido em ESCALARES, COMPOSTOS E ESPECIAIS.
 
 
         ESCALARES
@@ -16,7 +16,7 @@
         -bool ou boolean (sistema boleano de verdadeiro ou falso
 
 
-        COMPOSTO
+        COMPOSTOS
         -Array (arranjo)
         -object
 
@@ -26,29 +26,85 @@
         -callabe
         -mixed(introduzido no php 8.0, mistura todos os tipos primitivos.)
         
-        )
+        
      -->
 </head>
 <body>
     <?php
-    //0x = hexadecimal 0b = binario 0=Octal
-        // $num = 0b010101;
+        // EXERCICIO TIPOS DE VARIAVEIS 
+
+        //0x = hexadecimal 0b = binario 0=Octal
+        //
+        // $num = 0b010101; // como iniciou com 0b, num é uma variavem do tipo binario.
         // echo "O valor da variavel é $num";
+
+
+        //EXERCICIO MOSTRAR O  TIPO DE VARIAVEL ATRAVES DA FUNÇÃO var_dump()
+        //
         // $v = 300;
-        // var_dump($v); mostra o tipo de variavel
+        // var_dump($v); //mostra o tipo de variavel
+        
+        
         // $num = 3e2; //3x10(^2)
         // echo "variavel é $num"
         // $num = (integer)"650";
         // var_dump($num);
+
+        //EXERCICIO CRIAÇÃO DE ARRAY - TIPOS PRIMITIVO COMPOSTOS
+        //
         // $vet = [6, 2.6, "Maria", 3, False];
         // var_dump($vet);
 
-        class pessoa{
-            private string $nome;
-        }
-        $p = new Pessoa;
-        var_dump($p)
+        //EXERCICIO CRIAÇÃO DE OBJECT - TIPOS PRIMITIVO COMPOSTOS
+        //
+        // class pessoa{
+        //              private string $nome;
+        //             }
+        // $p = new Pessoa;
+        // var_dump($p)
+
+
+        //EXERCICIO DE USO DE ASPAS EM UM UNICO ECHO - UTILIZANDO ESCAPE SEQUENCES
+        //
+        // $nome = "Matheus";
+        // $sobrenome = " Alves" ;
+        // echo "$nome \"Coronel\" $sobrenome"; 
+        //    
+        // usar contra barra (\) antes para representar algo de valor semantico, no php se chama sequencia de escape ou escape sequences
+        //
+        // TIPOS DE ESCAPE SEQUENCES 
+        //
+        // Nova linha            -      \n
+        // Tabulação Horizontal  -      \t
+        // Barra invertida       -      \\
+        // Sinal de cifrão       -      \$
+        // Codepoint Unicode     -      \u{}
+
+        
+        // FORMATOS DE STRING: 1-double quoted "aspas duplas" 2-single quoted 'aspas simples' 3-String Heredoc 4-String Nowdoc.
+      
+        // STRING DOUBLE QUOTED (ASPAS DUPLAS):
+        // echo "$nome $sobrenome" >>>> Matheus Alves // sintaxe é compreendida
+     
+        // STRING SINGLE QUOTED ( ASPAS SIMPLES):
+        // echo '$nome $sobrenome" >>>> $nome $sobrenome // php não entende semantica e printa ao pé da letra.
+        
+        //STRING HEREDOC - Caracteristicias de aspas duplas e pode colocar texto em multiplas linhas. vantagem: quebrar linha em formato texto.Não funciona em HTML.
+        // 
+        // $curso = "Curso em video PHP";
+        // $ano = date ("Y");
+        // echo <<< FRASE
+        //
+        // Estou estudando 
+        //     $curso 
+        //         em $ano
+        // FRASE;
+
+        //STRING NOWDOC- Caracteristicias de aspas simples (nao contem interpretação.) e pode colocar texto em multiplas linhas. vantagem: quebrar linha em formato texto.Não funciona em HTML.
+
+        
+
     ?>
-    
+
 </body>
 </html>
